1647d7a33d739b11c5fd2c575c6db470949e54dd
Jacob Pratt <the.z.cuber@gmail.com>
2018-09-20 22:31:10 -0400

    Allow burst requests
    By default, requests are rate limited after 50% of the requests for a
    duration are sent. This can easily be changed in the future by modifying
    the global variable if necessary,

--- src/RateLimit.ts
+++ src/RateLimit.ts
@@ -3,12 +3,18 @@ interface Node {
   next?: Node;
 }
 
+// only rate limit requests after we hit half of our limit
+const limit_after_percent = 0.5;
+
 export default class RateLimit {
   private readonly delay_ms: number;
   private head: Option<Node>;
   private priority_tail: Option<Node>;
   private tail: Option<Node>;
   private is_running = false;
+
+  private requests_made = 0;
+  private readonly requests_allowed: number;
 
   constructor(interval: string);
   constructor(per_interval: number, interval: string);
@@ -18,22 +24,28 @@ export default class RateLimit {
       per_interval = parseInt(per_interval, 10);
     }
 
+    let interval_ms;
     switch (interval) {
       case 'per second':
-        this.delay_ms = 1_000 / per_interval;
+        interval_ms = 1_000;
         break;
       case 'per minute':
-        this.delay_ms = 60_000 / per_interval;
+        interval_ms = 60_000;
         break;
       case 'per hour':
-        this.delay_ms = 3_600_000 / per_interval;
+        interval_ms = 3_600_000;
         break;
       case 'per day':
-        this.delay_ms = 86_400_000 / per_interval;
+        interval_ms = 86_400_000;
         break;
       default:
         throw new Error('Invalid duration');
     }
+
+    this.delay_ms = interval_ms / per_interval / limit_after_percent;
+
+    this.requests_allowed = per_interval;
+    setInterval(() => (this.requests_made = 0), interval_ms);
   }
 
   public push(fn: () => unknown, priority = false): this {
@@ -63,8 +75,13 @@ export default class RateLimit {
     this.is_running = true;
 
     while (this.head !== undefined) {
+      this.requests_made++;
       this.head.fn();
-      await new Promise(resolve => setTimeout(resolve, this.delay_ms));
+
+      if (this.requests_made / this.requests_allowed >= limit_after_percent) {
+        await new Promise(resolve => setTimeout(resolve, this.delay_ms));
+      }
+
       this.head = this.head.next;
     }
 
